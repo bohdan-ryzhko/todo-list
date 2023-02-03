8c8ed7022f0cfdcb450f5fb9c0d75d0beef6ff31
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-02-03 16:24:50 +0200

    add refs of DOM

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+const refs = {
+	todoList: document.getElementById('todo-list-js'),
+	myInput: document.getElementById('todo-input'),
+	addBtn: document.querySelector('.add-item-btn'),
+	sortBtn: document.querySelector('.sort-items-btn'),
+	sortArrow: document.querySelector('.sort-arrow'),
+	clearBtn: document.querySelector('.clear-items-btn'),
+	addAlert: document.querySelector('.add-alert'),
+	oneAlert: document.querySelector('.one-alert'),
+	emptyAlert: document.querySelector('.empty-alert'),
+	myULItem: document.querySelectorAll('.todo-list__item'),
+	closeBtn: document.getElementsByClassName('close'),
+}
+
 const rendomBG = [
 	'#B15B00',
 	'#4E8397',
@@ -13,3 +27,5 @@ function generateRandomColorOfBody(list) {
 	const rand = min - 0.5 + Math.random() * (max - min + 1);
 	document.body.style.backgroundColor = list[Math.round(rand)];
 }
+
+generateRandomColorOfBody(rendomBG);
