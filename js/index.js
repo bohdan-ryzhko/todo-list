c51416c07a07feb9f9d7d8f7206fc9f960a5b4ec
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-02-03 16:28:59 +0200

    add function convertTime

--- js/index.js
+++ js/index.js
@@ -59,4 +59,18 @@ function showNotification(notification) {
 	}, 4000);
 }
 
+function convertTime(ms) {
+	const d = new Date();
+	const year = d.getFullYear();
+	const month = d.getMonth();
+	const date = d.getDate();
+	const daysms = ms % (24*60*60*1000);
+	const hours = Math.floor(daysms / (60*60*1000));
+	const hoursms = ms % (60*60*1000);
+	const minutes = Math.floor(hoursms / (60*1000));
+	const minutesms = ms % (60*1000);
+	const sec = Math.floor(minutesms / 1000);
+	return {year, month, date, hours, minutes, sec}
+}
+
 refs.addBtn.addEventListener('click', addListItem);
