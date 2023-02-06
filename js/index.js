0ecabd34b869fc874f90921b8f0ae44b228d7de3
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-02-06 20:28:02 +0200

    update function convertTime

--- js/index.js
+++ js/index.js
@@ -180,8 +180,7 @@ function convertTime(ms) {
 	const year = d.getFullYear();
 	const month = d.getMonth();
 	const date = d.getDate();
-	const daysms = ms % (24*60*60*1000);
-	const hours = Math.floor(daysms / (60*60*1000));
+	const hours = d.getHours();
 	const hoursms = ms % (60*60*1000);
 	const minutes = Math.floor(hoursms / (60*1000));
 	const minutesms = ms % (60*1000);
@@ -358,7 +357,6 @@ function addLeadingZero(value) {
 	return String(value).padStart(2, '0');
 }
 
-// console.log(localStorage);
 refs.addBtn.addEventListener('click', addListItem);
 refs.todoList.addEventListener('click', onListItem);
 refs.sortBtn.addEventListener('click', sortItems);
